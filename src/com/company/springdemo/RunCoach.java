ef45b3ed511cb8caf0b11433ef55f2c10bdb05bf
wangzanquan <wangzanquan@yahoo.com>
2018-08-16 16:59:51 -0400

    Setter Injection

--- src/com/company/springdemo/RunCoach.java
+++ src/com/company/springdemo/RunCoach.java
@@ -7,12 +7,28 @@ import org.springframework.stereotype.Component;
 public class RunCoach implements Coach {
 
 	private FortuneService fortuneService;
-	
-	//Constructor
+	/*
+	 
+	//Constructor Injection
 	@Autowired
 	public RunCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	*/
+	
+	//define a default constructor
+	public RunCoach() {
+		System.out.println("RunCoach: inside default constructor");
+	}
+	
+	//define a setter method
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println("RunCoach: inside setFortuneService");
+
+		this.fortuneService = theFortuneService;
+	}
+	
 	
 	@Override
 	public String getWorkOut() {
